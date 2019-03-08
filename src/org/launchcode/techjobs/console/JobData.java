b8d0c0c38ff21f2f97df8802fa0b15a352228708
h-packard <hilary.packard@gmail.com>
2019-03-08 17:52:27 -0600

    all requirements met

--- src/org/launchcode/techjobs/console/JobData.java
+++ src/org/launchcode/techjobs/console/JobData.java
@@ -10,6 +10,9 @@ import java.io.Reader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Set;
+//import java.util.Map;
+
 
 /**
  * Created by LaunchCode
@@ -53,7 +56,29 @@ public class JobData {
 
         return allJobs;
     }
+    //findByValue
+    public static ArrayList<HashMap<String,String>> findByValue(String value) {
+        loadData();
 
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+        for (HashMap<String, String> aJob: allJobs) {
+
+            for (HashMap.Entry jobEntry : aJob.entrySet()) {
+                String entryValue = String.valueOf(jobEntry.getValue());
+                entryValue = entryValue.toLowerCase();
+
+                if (entryValue.contains(value)) {
+                    jobs.add(aJob);
+
+                } else {
+                    continue;
+                }
+            }
+        }
+
+
+        return jobs;
+    }
     /**
      * Returns results of search the jobs data by key/value, using
      * inclusion of the search term.
@@ -62,7 +87,7 @@ public class JobData {
      * with "Enterprise Holdings, Inc".
      *
      * @param column   Column that should be searched.
-     * @param value Value of teh field to search for
+     * @param value Value of the field to search for
      * @return List of all jobs matching the criteria
      */
     public static ArrayList<HashMap<String, String>> findByColumnAndValue(String column, String value) {
@@ -75,6 +100,7 @@ public class JobData {
         for (HashMap<String, String> row : allJobs) {
 
             String aValue = row.get(column);
+            aValue = aValue.toLowerCase();
 
             if (aValue.contains(value)) {
                 jobs.add(row);
